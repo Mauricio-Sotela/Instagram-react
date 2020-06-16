6e07fc0c0baae868f0fd7be1b005c949496e4308
sotela88 <j-ms-27@hotmail.com>
2020-06-16 10:42:21 +0200

    insta

---
+++ build/precache-manifest.a57edd0aaccfb30e8c70fc7387d0e542.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f087fa2f53b177e9ff3ecf496259421",
+    "url": "/index.html"
+  },
+  {
+    "revision": "100abda01005710f2428",
+    "url": "/static/css/main.27c1270b.chunk.css"
+  },
+  {
+    "revision": "af3e8e9655c6fc7a30f9",
+    "url": "/static/js/2.2259ad6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2259ad6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "100abda01005710f2428",
+    "url": "/static/js/main.43dc60a4.chunk.js"
+  },
+  {
+    "revision": "76fc8598c3920fae7b69",
+    "url": "/static/js/runtime-main.71864b05.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1 className="container">Hi I am React</h1>
+      </React.Fragment>
+    );
+  }
+}
