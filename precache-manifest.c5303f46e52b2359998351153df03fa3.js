052594eb7f829fe8f42f6637900895b501ade40d
sotela88 <j-ms-27@hotmail.com>
2020-08-18 18:10:53 +0200

    Updates

---
+++ precache-manifest.c5303f46e52b2359998351153df03fa3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dbf40750c144ad2010e5c7e063dd255",
+    "url": "/Instagram-react/index.html"
+  },
+  {
+    "revision": "6166745ef10ff67f6e0d",
+    "url": "/Instagram-react/static/css/2.cc5754b7.chunk.css"
+  },
+  {
+    "revision": "66b72145899e05f8f33c",
+    "url": "/Instagram-react/static/css/main.6000bb5a.chunk.css"
+  },
+  {
+    "revision": "6166745ef10ff67f6e0d",
+    "url": "/Instagram-react/static/js/2.579f55d4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Instagram-react/static/js/2.579f55d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66b72145899e05f8f33c",
+    "url": "/Instagram-react/static/js/main.9beb989e.chunk.js"
+  },
+  {
+    "revision": "d1d514caabff52d1eaef",
+    "url": "/Instagram-react/static/js/runtime-main.54ffc617.js"
+  }
+]);
